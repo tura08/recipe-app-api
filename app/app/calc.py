391c29fb2de27c775f95c65d4d103501a21fa1d1
tura08 <126748501+tura08@users.noreply.github.com>
2025-04-06 11:28:40 +0200

    Fixed spacing bug for flake8

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator function
 """
 
+
 def add(x, y):
     """Add x, y and return result"""
     return x + y
+
 
 def subtract(x, y):
     return x - y
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,15 +13,15 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-    
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if db ready."""
         patched_check.return_value = True
-        
+
         call_command('wait_for_db')
-        
+
         patched_check.assert_called_once_with(databases=['default'])
-    
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting operational error"""
